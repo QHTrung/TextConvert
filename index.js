b6cd94d88f2675ed6617ff2f56cdacaea611dca7
QHTrung <qhtrung23072000@gmail.com>
2024-04-25 17:22:06 +0700

    init text convert project

---
+++ index.js
@@ -0,0 +1,51 @@
+const textInput = document.getElementById('text-input');
+const upperCaseBtn = document.querySelector('.upper-case-btn');
+const lowerCaseBtn = document.querySelector('.lower-case-btn');
+const capitalizedCaseBtn = document.querySelector('.capitalized-case-btn');
+const copyBtn = document.querySelector('.copy-btn');
+const clearBtn = document.querySelector('.clear-btn');
+const tooltip = document.getElementById('myTooltip');
+
+function checkEmpty(text) {
+  return text.length === 0;
+}
+upperCaseBtn.addEventListener('click', () => {
+  const inputValue = textInput.value;
+  if (checkEmpty(inputValue)) {
+    alert('Empty string. Please fill the string!');
+  } else {
+    textInput.value = inputValue.toUpperCase();
+  }
+});
+lowerCaseBtn.addEventListener('click', () => {
+  const inputValue = textInput.value;
+  if (checkEmpty(inputValue)) {
+    alert('Empty string. Please fill the string!');
+  } else {
+    textInput.value = inputValue.toLowerCase();
+  }
+});
+capitalizedCaseBtn.addEventListener('click', () => {
+  const inputValue = textInput.value;
+  if (checkEmpty(inputValue)) {
+    alert('Empty string. Please fill the string!');
+  } else {
+    textInput.value = inputValue
+      .toLowerCase()
+      .split(' ')
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ');
+  }
+});
+copyBtn.addEventListener('mouseout', () => {
+  tooltip.innerHTML = 'Copy to clipboard';
+});
+copyBtn.addEventListener('click', () => {
+  textInput.select();
+  textInput.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(textInput.value);
+  tooltip.innerHTML = 'Copied';
+});
+clearBtn.addEventListener('click', () => {
+  textInput.value = '';
+});
